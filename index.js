af918c208e4b5ca7b96f5d3eed6302629e438b61
ElenIva <ai93035@gmail.com>
2024-07-14 20:31:58 +0300

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+function changeBackgroundHappy() {
+    document.getElementById("gift-img-happy").style.backgroundImage = "url(images/happy.gif)";
+ }
+
+ function changeBackgroundCool() {
+    document.getElementById("gift-img-cool").style.backgroundImage = "url(images/cool.gif)"
+ }
+
+ function changeBackgroundHandsome() {
+    document.getElementById("gift-img-handsome").style.backgroundImage = "url(images/handsome.gif)"
+ }
+
+ function changeBackgroundCheers() {
+    document.getElementById("gift-img-cheers").style.backgroundImage = "url(images/woo-hoo.gif)"
+ }
